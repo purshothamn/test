b8156042bf02f520dfaeb57afc616e8f457d262a
purshothamn <53594281+purshothamn@users.noreply.github.com>
2021-02-20 22:06:19 +0530

    Update lPopgmUpdate.py

--- lPopgmUpdate.py
+++ lPopgmUpdate.py
@@ -1,48 +1,48 @@
-import json,sys
-from requests_html import HTML, HTMLSession
-from bs4 import BeautifulSoup
+# import json,sys
+# from requests_html import HTML, HTMLSession
+# from bs4 import BeautifulSoup
 
-to = ['purshoth27@gmail.com','athishpv2525@gmail.com']
+# to = ['purshoth27@gmail.com','athishpv2525@gmail.com']
 
-path = r'/home/vsts/test/test'
+# path = r'/home/vsts/test/test'
 
-sys.path.insert(1,r'/home/vsts/test/test/Google_Drive_API')
-from Google_Drive_API import Google_Drive_API
+# sys.path.insert(1,r'/home/vsts/test/test/Google_Drive_API')
+# from Google_Drive_API import Google_Drive_API
 
-Google_Drive_API.download('1TiZAwS_7Bs1X3oxBy9fic3JyGUl6VBIB','gmStat.json')
+# Google_Drive_API.download('1TiZAwS_7Bs1X3oxBy9fic3JyGUl6VBIB','gmStat.json')
     
-with open(path+'/gmStat.json') as json_read:
-    jsData = json.load(json_read)
+# with open(path+'/gmStat.json') as json_read:
+#     jsData = json.load(json_read)
 
-session = HTMLSession()
+# session = HTMLSession()
 
-src = session.get(url='https://steamunlocked.net/all-games/')
+# src = session.get(url='https://steamunlocked.net/all-games/')
 
-soup = BeautifulSoup(src.content, 'lxml')
+# soup = BeautifulSoup(src.content, 'lxml')
 
-sol = soup.find_all('div',attrs={'class':"info"})
+# sol = soup.find_all('div',attrs={'class':"info"})
 
-solList = []
+# solList = []
 
-checker = None
-for s in sol:
-    i = s.find('a').text.replace('\n','').replace('\t','').replace('’','')
-# for i in solList:
-    if i in jsData['Name']:
-        pass
-    else:
-        checker = True
-        sys.path.insert(1,r'/home/vsts/test/test/Gmail_API')
+# checker = None
+# for s in sol:
+#     i = s.find('a').text.replace('\n','').replace('\t','').replace('’','')
+# # for i in solList:
+#     if i in jsData['Name']:
+#         pass
+#     else:
+#         checker = True
+#         sys.path.insert(1,r'/home/vsts/test/test/Gmail_API')
 
-        from Gmail_API import Gmail_API
-        sendMail = Gmail_API.createMail
+#         from Gmail_API import Gmail_API
+#         sendMail = Gmail_API.createMail
 
-        jsData['Name'].insert(0,i)
-        link = s.find('a').attrs['href']
-        body = f'{i} is added to Popular, here\'s the link: {link}'
-        subject = 'New Popular Game'
-        sendMail(to,body,Subject=subject)
+#         jsData['Name'].insert(0,i)
+#         link = s.find('a').attrs['href']
+#         body = f'{i} is added to Popular, here\'s the link: {link}'
+#         subject = 'New Popular Game'
+#         sendMail(to,body,Subject=subject)
 
-if checker:
-    with open(path+'/gmStat.json','w') as json_write:
-        json.dump(jsData,json_write)
+# if checker:
+#     with open(path+'/gmStat.json','w') as json_write:
+#         json.dump(jsData,json_write)
